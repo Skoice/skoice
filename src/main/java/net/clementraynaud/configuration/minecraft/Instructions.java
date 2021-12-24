2f291e40e7f1cac75ffd1b6765785c52e4f07030
carlodrift <30211659+carlodrift@users.noreply.github.com>
2021-12-24 15:31:17 +0100

    Update of the license notice

--- src/main/java/net/clementraynaud/configuration/minecraft/Instructions.java
+++ src/main/java/net/clementraynaud/configuration/minecraft/Instructions.java
@@ -1,21 +1,22 @@
-// Copyright 2020, 2021 Clément "carlodrift" Raynaud, Lucas "Lucas_Cdry" Cadiry and contributors
-// Copyright 2016, 2017, 2018, 2019, 2020, 2021 Austin "Scarsz" Shapiro
-
-// This file is part of Skoice.
-
-// Skoice is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// Skoice is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with Skoice.  If not, see <https://www.gnu.org/licenses/>.
-
+/*
+ * Copyright 2020, 2021 Clément "carlodrift" Raynaud, Lucas "Lucas_Cdry" Cadiry and contributors
+ * Copyright 2016, 2017, 2018, 2019, 2020, 2021 Austin "Scarsz" Shapiro
+ *
+ * This file is part of Skoice.
+ *
+ * Skoice is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Skoice is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Skoice.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 package net.clementraynaud.configuration.minecraft;
 
