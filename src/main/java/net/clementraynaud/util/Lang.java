dc466c76e2ab32078d8231c89f863bb9d4ed85d1
LucasCdry <lucas.cadiry@gmail.com>
2022-02-15 13:43:09 +0100

    Remove any §

--- src/main/java/net/clementraynaud/util/Lang.java
+++ src/main/java/net/clementraynaud/util/Lang.java
@@ -20,6 +20,8 @@
 
 package net.clementraynaud.util;
 
+import org.bukkit.ChatColor;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -38,31 +40,31 @@ public class Lang {
             put("FR", "Plugin désactivé !");
         }}),
         OUTDATED_VERSION_WARNING(new HashMap<String, String>() {{
-            put("EN", "§eYou are using an outdated version ({runningVersion}). Download the latest version ({latestVersion}) here: §bhttps://www.spigotmc.org/resources/skoice-proximity-voice-chat.82861/§e.");
-            put("FR", "§eVous utilisez une version obsolète ({runningVersion}). Téléchargez la dernière version ({latestVersion}) ici : §bhttps://www.spigotmc.org/resources/skoice-proximity-voice-chat.82861/§e.");
+            put("EN", ChatColor.YELLOW + "You are using an outdated version ({runningVersion}). Download the latest version ({latestVersion}) here: " + ChatColor.AQUA + "https://www.spigotmc.org/resources/skoice-proximity-voice-chat.82861/" + ChatColor.YELLOW + ".");
+            put("FR", ChatColor.YELLOW + "Vous utilisez une version obsolète ({runningVersion}). Téléchargez la dernière version ({latestVersion}) ici : " + ChatColor.AQUA + "https://www.spigotmc.org/resources/skoice-proximity-voice-chat.82861/" + ChatColor.YELLOW + ".");
         }}),
         NO_TOKEN_WARNING(new HashMap<String, String>() {{
-            put("EN", "§eToken not set, join your Minecraft server to set up Skoice.");
-            put("FR", "§eToken non défini, rejoignez votre serveur Minecraft pour configurer Skoice.");
+            put("EN", ChatColor.YELLOW + "Token not set, join your Minecraft server to set up Skoice.");
+            put("FR", ChatColor.YELLOW + "Token non défini, rejoignez votre serveur Minecraft pour configurer Skoice.");
         }}),
         NO_LANGUAGE_WARNING(new HashMap<String, String>() {{
-            put("EN", "§eLanguage not set, type \"/configure\" on your Discord server to set up Skoice.");
+            put("EN", ChatColor.YELLOW + "Language not set, type \"/configure\" on your Discord server to set up Skoice.");
         }}),
         MULTIPLE_GUILDS_WARNING(new HashMap<String, String>() {{
-            put("EN", "§eYour bot is on multiple Discord servers, type \"/configure\" on your Discord server to choose one.");
-            put("FR", "§eVotre bot est sur plusieurs serveurs Discord, tapez \"/configure\" sur votre serveur Discord pour en choisir un.");
+            put("EN", ChatColor.YELLOW + "Your bot is on multiple Discord servers, type \"/configure\" on your Discord server to choose one.");
+            put("FR", ChatColor.YELLOW + "Votre bot est sur plusieurs serveurs Discord, tapez \"/configure\" sur votre serveur Discord pour en choisir un.");
         }}),
         NO_LOBBY_ID_WARNING(new HashMap<String, String>() {{
-            put("EN", "§eLobby not set, type \"/configure\" on your Discord server to set up Skoice.");
-            put("FR", "§eSalon vocal principal non défini, tapez \"/configure\" sur votre serveur Discord pour configurer Skoice.");
+            put("EN", ChatColor.YELLOW + "Lobby not set, type \"/configure\" on your Discord server to set up Skoice.");
+            put("FR", ChatColor.YELLOW + "Salon vocal principal non défini, tapez \"/configure\" sur votre serveur Discord pour configurer Skoice.");
         }}),
         NO_DISTANCES_WARNING(new HashMap<String, String>() {{
-            put("EN", "§eRadius not set, join your Minecraft server to set up Skoice.");
-            put("FR", "§eRayons non définis, tapez \"/configure\" sur votre serveur Discord pour configurer Skoice.");
+            put("EN", ChatColor.YELLOW + "Radius not set, join your Minecraft server to set up Skoice.");
+            put("FR", ChatColor.YELLOW + "Rayons non définis, tapez \"/configure\" sur votre serveur Discord pour configurer Skoice.");
         }}),
         MISSING_ACCESS_ERROR(new HashMap<String, String>() {{
-            put("EN", "§cYou did not grant your bot the permission to register commands on your Discord server (\"applications.commands\").");
-            put("FR", "§cVous n'avez pas accordé à votre bot la permission de déclarer des commandes sur votre serveur Discord (\"applications.commands\").");
+            put("EN", ChatColor.RED + "You did not grant your bot the permission to register commands on your Discord server (\"applications.commands\").");
+            put("FR", ChatColor.RED + "Vous n'avez pas accordé à votre bot la permission de déclarer des commandes sur votre serveur Discord (\"applications.commands\").");
         }});
 
         private final Map<String, String> languageMessageMap;
@@ -76,7 +78,7 @@ public class Lang {
         }
     }
 
-    public enum Discord {
+    public enum Discord {;
 
         private final Map<String, String> languageMessageMap;
 
@@ -92,45 +94,47 @@ public class Lang {
     public enum Minecraft {
 
         ILLEGAL_EXECUTOR(new HashMap<String, String>() {{
-            put("EN", "§dSkoice §8• §7This command is §conly executable §7by players.");
-            put("FR", "§dSkoice §8• §7Cette commande est §cseulement exécutable §7par les joueurs.");
+            put("EN", CHAT_PREFIX + "This command is " + ChatColor.RED + "only executable " + ChatColor.GRAY + "by players.");
+            put("FR", CHAT_PREFIX + "Cette commande est " + ChatColor.RED + "seulement exécutable " + ChatColor.GRAY + "par les joueurs.");
         }}),
         NOT_CONFIGURED_CORRECTLY_INTERACTIVE(new HashMap<String, String>() {{
             put("EN", "?");
             put("FR", "?");
         }}),
         NOT_CONFIGURED_CORRECTLY_COMMAND(new HashMap<String, String>() {{
-            put("EN", "§dSkoice §8• §7Skoice is §cnot configured correctly§7. Type \"§e/configure§7\" to set it up.");
-            put("FR", "§dSkoice §8• §7Skoice n'est §cpas configuré correctement§7. Tapez \"§e/configure§7\" pour le configurer.");
+            put("EN", CHAT_PREFIX + "Skoice is " + ChatColor.RED + "not configured correctly" + ChatColor.GRAY + ". Type \"" + ChatColor.YELLOW + "/configure" + ChatColor.GRAY + "\" to set it up.");
+            put("FR", CHAT_PREFIX + "Skoice n'est " + ChatColor.RED + "pas configuré correctement" + ChatColor.GRAY + ". Tapez \"" + ChatColor.YELLOW + "/configure" + ChatColor.GRAY + "\" pour le configurer.");
         }}),
         NOT_CONFIGURED_CORRECTLY_DISCORD(new HashMap<String, String>() {{
-            put("EN", "§dSkoice §8• §7Skoice is §cnot configured correctly§7. Type \"§e/configure§7\" on your Discord server to set it up.");
-            put("FR", "§dSkoice §8• §7Skoice n'est §cpas configuré correctement§7. Tapez \"§e/configure§7\" sur votre serveur Discord pour le configurer.");
+            put("EN", CHAT_PREFIX + "Skoice is " + ChatColor.RED + "not configured correctly" + ChatColor.GRAY + ". Type \"" + ChatColor.YELLOW + "/configure" + ChatColor.GRAY + "\" on your Discord server to set it up.");
+            put("FR", CHAT_PREFIX + "Skoice n'est " + ChatColor.RED + "pas configuré correctement" + ChatColor.GRAY + ". Tapez \"" + ChatColor.YELLOW + "/configure" + ChatColor.GRAY + "\" sur votre serveur Discord pour le configurer.");
         }}),
         BOT_CREATION_INTERACTIVE(new HashMap<String, String>() {{
             put("EN", "?");
             put("FR", "?");
         }}),
         BOT_CREATION_LINK(new HashMap<String, String>() {{
-            put("EN", " \n§dSkoice §8• §7Configuration (§fBot Creation§7)\n \n§8• §7First, you need to create a bot and invite it to your Discord server. Please follow the instructions on this page: §bhttps://github.com/carlodrift/skoice/wiki§7.\n§8• §7When you have retrieved its token, type \"§e/token§7\" followed by the token.\n§8• §7Once done, type \"§e/configure§7\" on your Discord server to go to the next step.\n ");
-            put("FR", " \n§dSkoice §8• §7Configuration (§fCréation du bot§7)\n \n§8• §7Tout d'abord, vous devez créer un bot et l'inviter sur votre serveur Discord. Merci de suivre les instructions sur cette page: §bhttps://github.com/carlodrift/skoice/wiki§7.\n§8• §7Une fois son token récupéré, tapez \"§e/token§7\" suivi du token.\n§8• §7Une fois fait, tapez \"§e/configure§7\" sur votre serveur Discord pour passer à l'étape suivante.\n ");
+            put("EN", " \n" + CHAT_PREFIX + "Configuration (" + ChatColor.WHITE + "Bot Creation" + ChatColor.GRAY + ")\n \n" + ChatColor.DARK_GRAY + "• " + ChatColor.GRAY + "First, you need to create a bot and invite it to your Discord server. Please follow the instructions on this page: " + ChatColor.AQUA + "https://github.com/carlodrift/skoice/wiki" + ChatColor.GRAY + ".\n" + ChatColor.DARK_GRAY + "• " + ChatColor.GRAY + "When you have retrieved its token, type \"" + ChatColor.YELLOW + "/token" + ChatColor.GRAY + "\" followed by the token.\n" + ChatColor.DARK_GRAY + "• " + ChatColor.GRAY + "Once done, type \"" + ChatColor.YELLOW + "/configure" + ChatColor.GRAY + "\" on your Discord server to go to the next step.\n ");
+            put("FR", " \n" + CHAT_PREFIX + "Configuration (" + ChatColor.WHITE + "Création du bot" + ChatColor.GRAY + ")\n \n" + ChatColor.DARK_GRAY + "• " + ChatColor.GRAY + "Tout d'abord, vous devez créer un bot et l'inviter sur votre serveur Discord. Merci de suivre les instructions sur cette page: " + ChatColor.AQUA + "https://github.com/carlodrift/skoice/wiki" + ChatColor.GRAY + ".\n" + ChatColor.DARK_GRAY + "• " + ChatColor.GRAY + "Une fois son token récupéré, tapez \"" + ChatColor.YELLOW + "/token" + ChatColor.GRAY + "\" suivi du token.\n" + ChatColor.DARK_GRAY + "• " + ChatColor.GRAY + "Une fois fait, tapez \"" + ChatColor.YELLOW + "/configure" + ChatColor.GRAY + "\" sur votre serveur Discord pour passer à l'étape suivante.\n ");
         }}),
         ALREADY_CONFIGURED(new HashMap<String, String>() {{
-            put("EN", "§dSkoice §8• §7Skoice is §calready configured§7. Type \"§e/configure§7\" on your Discord server to update its settings.");
-            put("FR", "§dSkoice §8• §7Skoice est §cdéjà configuré§7. Tapez \"§e/configure§7\" sur votre serveur Discord pour mettre à jour ses paramètres.");
+            put("EN", CHAT_PREFIX + "Skoice is " + ChatColor.RED + "already configured" + ChatColor.GRAY + ". Type \"" + ChatColor.YELLOW + "/configure" + ChatColor.GRAY + "\" on your Discord server to update its settings.");
+            put("FR", CHAT_PREFIX + "Skoice est " + ChatColor.RED + "déjà configuré" + ChatColor.GRAY + ". Tapez \"" + ChatColor.YELLOW + "/configure" + ChatColor.GRAY + "\" sur votre serveur Discord pour mettre à jour ses paramètres.");
         }}),
         NO_TOKEN(new HashMap<String, String>() {{
-            put("EN", "§dSkoice §8• §7You have §cnot provided a token§7.");
-            put("FR", "§dSkoice §8• §7Vous n'avez §cpas fourni de token§7.");
+            put("EN", CHAT_PREFIX + "You have " + ChatColor.RED + "not provided a token" + ChatColor.GRAY + ".");
+            put("FR", CHAT_PREFIX + "Vous n'avez " + ChatColor.RED + "pas fourni de token" + ChatColor.GRAY + ".");
         }}),
         INVALID_TOKEN(new HashMap<String, String>() {{
-            put("EN", "§dSkoice §8• §7You have §cnot provided a valid token§7.");
-            put("FR", "§dSkoice §8• §7Vous n'avez §cpas fourni de token valide§7.");
+            put("EN", CHAT_PREFIX + "You have " + ChatColor.RED + "not provided a valid token" + ChatColor.GRAY + ".");
+            put("FR", CHAT_PREFIX + "Vous n'avez " + ChatColor.RED + "pas fourni de token valide" + ChatColor.GRAY + ".");
         }}),
         ALREADY_CONNECTED(new HashMap<String, String>() {{
-            put("EN", "§dSkoice §8• §7A bot is §calready connected§7. Restart your Minecraft server to apply the new token.");
-            put("FR", "§dSkoice §8• §7Un bot is §cdéjà connecté§7. Redémarrez votre serveur Minecraft pour appliquer le nouveau token.");
+            put("EN", CHAT_PREFIX + "A bot is " + ChatColor.RED + "already connected" + ChatColor.GRAY + ". Restart your Minecraft server to apply the new token.");
+            put("FR", CHAT_PREFIX + "Un bot is " + ChatColor.RED + "déjà connecté" + ChatColor.GRAY + ". Redémarrez votre serveur Minecraft pour appliquer le nouveau token.");
         }});
+
+        private static final String CHAT_PREFIX = ChatColor.LIGHT_PURPLE + "Skoice " + ChatColor.DARK_GRAY + "• " + ChatColor.GRAY;
 
         private final Map<String, String> languageMessageMap;
 
