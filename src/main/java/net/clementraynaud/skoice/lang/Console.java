220f50b4924cf6a4ba453be916c3fd867f62f8ec
carlodrift <30211659+carlodrift@users.noreply.github.com>
2022-02-16 18:21:57 +0100

    Redesign lang system

---
+++ src/main/java/net/clementraynaud/skoice/lang/Console.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2020, 2021 Clément "carlodrift" Raynaud, Lucas "Lucas_Cdry" Cadiry and contributors
+ * Copyright 2016, 2017, 2018, 2019, 2020, 2021 Austin "Scarsz" Shapiro
+ *
+ * This file is part of Skoice.
+ *
+ * Skoice is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Skoice is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Skoice.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.clementraynaud.skoice.lang;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import org.bukkit.ChatColor;
+
+import java.util.Map;
+
+import static net.clementraynaud.skoice.Skoice.getPlugin;
+
+public enum Console {
+    PLUGIN_ENABLED_INFO(Maps.newHashMap(ImmutableMap.of(
+            Lang.EN, "Plugin activé.",
+            Lang.FR, "Plugin enabled."))),
+
+    PLUGIN_DISABLED_INFO(Maps.newHashMap(ImmutableMap.of(
+            Lang.EN, "Plugin disabled.",
+            Lang.FR, "Plugin désactivé."))),
+
+    CONFIGURATION_COMPLETE_INFO(Maps.newHashMap(ImmutableMap.of(
+            Lang.EN, "Configuration complete. Type \"/link\" on your Discord server to link your Discord account to Minecraft.",
+            Lang.FR, "Configuration terminée. Tapez \"/link\" sur votre serveur Discord pour relier votre compte Discord à Minecraft."))),
+
+    BOT_CONNECTED_INFO(Maps.newHashMap(ImmutableMap.of(
+            Lang.EN, "Your bot is connected.",
+            Lang.FR, "Votre bot est connecté."))),
+
+    OUTDATED_VERSION_WARNING(Maps.newHashMap(ImmutableMap.of(
+            Lang.EN, ChatColor.YELLOW + "You are using an outdated version ({runningVersion}). Download the latest version ({latestVersion}) here: " + ChatColor.AQUA + "https://www.spigotmc.org/resources/skoice-proximity-voice-chat.82861/" + ChatColor.YELLOW + ".",
+            Lang.FR, ChatColor.YELLOW + "Vous utilisez une version obsolète ({runningVersion}). Téléchargez la dernière version ({latestVersion}) ici : " + ChatColor.AQUA + "https://www.spigotmc.org/resources/skoice-proximity-voice-chat.82861/" + ChatColor.YELLOW + "."))),
+
+    NO_TOKEN_WARNING(Maps.newHashMap(ImmutableMap.of(
+            Lang.EN, ChatColor.YELLOW + "Token not set. Join your Minecraft server to set up Skoice.",
+            Lang.FR, ChatColor.YELLOW + "Token non défini. Rejoignez votre serveur Minecraft pour configurer Skoice."))),
+
+    NO_LANGUAGE_WARNING(Maps.newHashMap(ImmutableMap.of(
+            Lang.EN, ChatColor.YELLOW + "Language not set. Type \"/configure\" on your Discord server to set up Skoice."))),
+
+    MULTIPLE_GUILDS_WARNING(Maps.newHashMap(ImmutableMap.of(
+            Lang.EN, ChatColor.YELLOW + "Your bot is on multiple Discord servers. Type \"/configure\" on your Discord server to choose one.",
+            Lang.FR, ChatColor.YELLOW + "Votre bot est sur plusieurs serveurs Discord. Tapez \"/configure\" sur votre serveur Discord pour en choisir un."))),
+
+    NO_LOBBY_ID_WARNING(Maps.newHashMap(ImmutableMap.of(
+            Lang.EN, ChatColor.YELLOW + "Lobby not set. Type \"/configure\" on your Discord server to set up Skoice.",
+            Lang.FR, ChatColor.YELLOW + "Salon vocal principal non défini. Tapez \"/configure\" sur votre serveur Discord pour configurer Skoice."))),
+
+    NO_DISTANCES_WARNING(Maps.newHashMap(ImmutableMap.of(
+            Lang.EN, ChatColor.YELLOW + "Radius not set. Type \"/configure\" on your Discord server to set up Skoice.",
+            Lang.FR, ChatColor.YELLOW + "Rayons non définis. Tapez \"/configure\" sur votre serveur Discord pour configurer Skoice."))),
+
+    MISSING_ACCESS_ERROR(Maps.newHashMap(ImmutableMap.of(
+            Lang.EN, ChatColor.RED + "You did not grant your bot the permission to register commands on your Discord server (\"applications.commands\").",
+            Lang.FR, ChatColor.RED + "Vous n'avez pas accordé à votre bot la permission de déclarer des commandes sur votre serveur Discord (\"applications.commands\")."))),
+
+    BOT_COULD_NOT_CONNECT_ERROR(Maps.newHashMap(ImmutableMap.of(
+            Lang.EN, ChatColor.RED + "Your bot could not connect. To update the token, type \"/token\" followed by the new token.",
+            Lang.FR, ChatColor.RED + "Votre bot n'a pas pu se connecter. Pour mettre à jour le token, tapez \"/token\" suivi du nouveau token."))),
+
+    UNEXPECTED_VALUE(Maps.newHashMap(ImmutableMap.of(
+            Lang.EN, "Unexpected value: {value}",
+            Lang.FR, "Valeur inattendue : {value}")));
+
+    final Map<Lang, String> messages;
+
+    Console(Map<Lang, String> messages) {
+        this.messages = messages;
+    }
+
+    @Override
+    public String toString() {
+        return messages.getOrDefault(Lang.valueOf(getPlugin().getConfigFile().getString("language")), messages.get(Lang.FR));
+    }
+}
---
+++ src/main/java/net/clementraynaud/skoice/lang/Minecraft.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2020, 2021 Clément "carlodrift" Raynaud, Lucas "Lucas_Cdry" Cadiry and contributors
+ * Copyright 2016, 2017, 2018, 2019, 2020, 2021 Austin "Scarsz" Shapiro
+ *
+ * This file is part of Skoice.
+ *
+ * Skoice is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Skoice is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Skoice.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.clementraynaud.skoice.lang;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import org.bukkit.ChatColor;
+
+import java.util.Map;
+
+import static net.clementraynaud.skoice.Skoice.getPlugin;
+import static net.clementraynaud.skoice.lang.Lang.PREFIX;
+
+public enum Minecraft {
+    ILLEGAL_EXECUTOR(Maps.newHashMap(ImmutableMap.of(
+            Lang.EN, PREFIX + "This command is " + ChatColor.RED + "only executable " + ChatColor.GRAY + "by players.",
+            Lang.FR, PREFIX + "Cette commande est " + ChatColor.RED + "seulement exécutable " + ChatColor.GRAY + "par les joueurs."))),
+
+    INCOMPLETE_CONFIGURATION(Maps.newHashMap(ImmutableMap.of(
+            Lang.EN, PREFIX + "Skoice is " + ChatColor.RED + "not configured correctly" + ChatColor.GRAY + ". Please contact an administrator.",
+            Lang.FR, PREFIX + "Skoice n'est " + ChatColor.RED + "pas configuré correctement" + ChatColor.GRAY + ". Veuillez contacter un administrateur."))),
+
+    INCOMPLETE_CONFIGURATION_OPERATOR_INTERACTIVE(Maps.newHashMap(ImmutableMap.of(
+            Lang.EN, "?"))),
+
+    INCOMPLETE_CONFIGURATION_OPERATOR_COMMAND(Maps.newHashMap(ImmutableMap.of(
+            Lang.EN, PREFIX + "Skoice is " + ChatColor.RED + "not configured correctly" + ChatColor.GRAY + ". Type \"" + ChatColor.YELLOW + "/configure" + ChatColor.GRAY + "\" to set it up.",
+            Lang.FR, PREFIX + "Skoice n'est " + ChatColor.RED + "pas configuré correctement" + ChatColor.GRAY + ". Tapez \"" + ChatColor.YELLOW + "/configure" + ChatColor.GRAY + "\" pour le configurer."))),
+
+    INCOMPLETE_CONFIGURATION_OPERATOR_DISCORD(Maps.newHashMap(ImmutableMap.of(
+            Lang.EN, PREFIX + "Skoice is " + ChatColor.RED + "not configured correctly" + ChatColor.GRAY + ". Type \"" + ChatColor.YELLOW + "/configure" + ChatColor.GRAY + "\" on your Discord server to set it up.",
+            Lang.FR, PREFIX + "Skoice n'est " + ChatColor.RED + "pas configuré correctement" + ChatColor.GRAY + ". Tapez \"" + ChatColor.YELLOW + "/configure" + ChatColor.GRAY + "\" sur votre serveur Discord pour le configurer."))),
+
+    BOT_CREATION_INTERACTIVE(Maps.newHashMap(ImmutableMap.of(
+            Lang.EN, "?"))),
+
+    BOT_CREATION_LINK(Maps.newHashMap(ImmutableMap.of(
+            Lang.EN, " \n" + PREFIX + "Configuration (" + ChatColor.WHITE + "Bot Creation" + ChatColor.GRAY + ")\n \n" + ChatColor.DARK_GRAY + "• " + ChatColor.GRAY + "First, you need to create a bot and invite it to your Discord server. Please follow the instructions on this page: " + ChatColor.AQUA + "https://github.com/carlodrift/skoice/wiki" + ChatColor.GRAY + ".\n" + ChatColor.DARK_GRAY + "• " + ChatColor.GRAY + "When you have retrieved its token, type \"" + ChatColor.YELLOW + "/token" + ChatColor.GRAY + "\" followed by the token.\n" + ChatColor.DARK_GRAY + "• " + ChatColor.GRAY + "Once done, type \"" + ChatColor.YELLOW + "/configure" + ChatColor.GRAY + "\" on your Discord server to go to the next step.\n ",
+            Lang.FR, " \n" + PREFIX + "Configuration (" + ChatColor.WHITE + "Création du bot" + ChatColor.GRAY + ")\n \n" + ChatColor.DARK_GRAY + "• " + ChatColor.GRAY + "Tout d'abord, vous devez créer un bot et l'inviter sur votre serveur Discord. Merci de suivre les instructions sur cette page: " + ChatColor.AQUA + "https://github.com/carlodrift/skoice/wiki" + ChatColor.GRAY + ".\n" + ChatColor.DARK_GRAY + "• " + ChatColor.GRAY + "Une fois son token récupéré, tapez \"" + ChatColor.YELLOW + "/token" + ChatColor.GRAY + "\" suivi du token.\n" + ChatColor.DARK_GRAY + "• " + ChatColor.GRAY + "Une fois fait, tapez \"" + ChatColor.YELLOW + "/configure" + ChatColor.GRAY + "\" sur votre serveur Discord pour passer à l'étape suivante.\n "))),
+
+    ALREADY_CONFIGURED(Maps.newHashMap(ImmutableMap.of(
+            Lang.EN, PREFIX + "Skoice is " + ChatColor.RED + "already configured" + ChatColor.GRAY + ". Type \"" + ChatColor.YELLOW + "/configure" + ChatColor.GRAY + "\" on your Discord server to update its settings.",
+            Lang.FR, PREFIX + "Skoice est " + ChatColor.RED + "déjà configuré" + ChatColor.GRAY + ". Tapez \"" + ChatColor.YELLOW + "/configure" + ChatColor.GRAY + "\" sur votre serveur Discord pour mettre à jour ses réglages."))),
+
+    NO_TOKEN(Maps.newHashMap(ImmutableMap.of(
+            Lang.EN, PREFIX + "You have " + ChatColor.RED + "not provided a token" + ChatColor.GRAY + ".",
+            Lang.FR, PREFIX + "Vous n'avez " + ChatColor.RED + "pas fourni de token" + ChatColor.GRAY + "."))),
+
+    INVALID_TOKEN(Maps.newHashMap(ImmutableMap.of(
+            Lang.EN, PREFIX + "You have " + ChatColor.RED + "not provided a valid token" + ChatColor.GRAY + ".",
+            Lang.FR, PREFIX + "Vous n'avez " + ChatColor.RED + "pas fourni de token valide" + ChatColor.GRAY + "."))),
+
+    BOT_CONNECTED(Maps.newHashMap(ImmutableMap.of(
+            Lang.EN, PREFIX + "Your bot is " + ChatColor.GREEN + "now connected" + ChatColor.GRAY + ".",
+            Lang.FR, PREFIX + "Votre bot est " + ChatColor.GREEN + "désormais connecté" + ChatColor.GRAY + "."))),
+
+    BOT_CONNECTED_INCOMPLETE_CONFIGURATION_DISCORD(Maps.newHashMap(ImmutableMap.of(
+            Lang.EN, PREFIX + "Your bot is " + ChatColor.GREEN + "now connected" + ChatColor.GRAY + ". Type \"" + ChatColor.YELLOW + "/configure" + ChatColor.GRAY + "\" on your Discord server to set it up.",
+            Lang.FR, PREFIX + "Votre bot est " + ChatColor.GREEN + "désormais connecté" + ChatColor.GRAY + ". Tapez \"" + ChatColor.YELLOW + "/configure" + ChatColor.GRAY + "\" sur votre serveur Discord pour le configurer."))),
+
+    BOT_ALREADY_CONNECTED(Maps.newHashMap(ImmutableMap.of(
+            Lang.EN, PREFIX + "A bot is " + ChatColor.RED + "already connected" + ChatColor.GRAY + ". Restart your Minecraft server to apply the new token.",
+            Lang.FR, PREFIX + "Un bot is " + ChatColor.RED + "déjà connecté" + ChatColor.GRAY + ". Redémarrez votre serveur Minecraft pour appliquer le nouveau token."))),
+
+    BOT_COULD_NOT_CONNECT(Maps.newHashMap(ImmutableMap.of(
+            Lang.EN, PREFIX + "Your bot " + ChatColor.RED + "could not connect" + ChatColor.GRAY + ". Try again with a valid token.",
+            Lang.FR, PREFIX + "Votre bot " + ChatColor.RED + "n'a pas pu se connecter" + ChatColor.GRAY + ". Essayez de nouveau avec un token valide."))),
+
+    ACCOUNT_LINKED(Maps.newHashMap(ImmutableMap.of(
+            Lang.EN, PREFIX + "You have " + ChatColor.GREEN + "linked your Minecraft account " + ChatColor.GRAY + "to Discord.",
+            Lang.FR, PREFIX + "Vous avez " + ChatColor.GREEN + "lié votre compte Minecraft " + ChatColor.GRAY + "à Discord."))),
+
+    ACCOUNT_UNLINKED(Maps.newHashMap(ImmutableMap.of(
+            Lang.EN, PREFIX + "You have " + ChatColor.RED + "unlinked your Minecraft account " + ChatColor.GRAY + "from Discord.",
+            Lang.FR, PREFIX + "Vous avez " + ChatColor.RED + "delié votre compte Minecraft " + ChatColor.GRAY + "de Discord."))),
+
+    ACCOUNT_NOT_LINKED(Maps.newHashMap(ImmutableMap.of(
+            Lang.EN, PREFIX + "You have " + ChatColor.RED + "not linked your Minecraft account " + ChatColor.GRAY + "to Discord. Type \"" + ChatColor.YELLOW + "/link" + ChatColor.GRAY + "\" on our Discord server to link it.",
+            Lang.FR, PREFIX + "Vous avez " + ChatColor.RED + "pas lié votre compte Minecraft " + ChatColor.GRAY + "à Discord. Tapez \"" + ChatColor.YELLOW + "/link" + ChatColor.GRAY + "\" sur notre serveur Discord pour le lier."))),
+
+    ACCOUNT_ALREADY_LINKED(Maps.newHashMap(ImmutableMap.of(
+            Lang.EN, PREFIX + "You have " + ChatColor.RED + "already linked your Minecraft account " + ChatColor.GRAY + "to Discord. Type \"" + ChatColor.YELLOW + "/unlink" + ChatColor.GRAY + "\" to unlink it.",
+            Lang.FR, PREFIX + "Vous avez " + ChatColor.RED + "déjà lié votre compte Minecraft " + ChatColor.GRAY + "à Discord. Tapez \"" + ChatColor.YELLOW + "/unlink" + ChatColor.GRAY + "\" pour le délier."))),
+
+    NO_CODE(Maps.newHashMap(ImmutableMap.of(
+            Lang.EN, PREFIX + "You have " + ChatColor.RED + "not provided a code" + ChatColor.GRAY + ". Type \"" + ChatColor.YELLOW + "/link" + ChatColor.GRAY + "\" on our Discord server to receive one.",
+            Lang.FR, PREFIX + "Vous n'avez " + ChatColor.RED + "pas fourni de code" + ChatColor.GRAY + ". Tapez \"" + ChatColor.YELLOW + "/link" + ChatColor.GRAY + "\" sur notre serveur Discord pour en recevoir un."))),
+
+    INVALID_CODE(Maps.newHashMap(ImmutableMap.of(
+            Lang.EN, PREFIX + "You have " + ChatColor.RED + "not provided a valid code" + ChatColor.GRAY + ". Type \"" + ChatColor.YELLOW + "/link" + ChatColor.GRAY + "\" on our Discord server to receive one.",
+            Lang.FR, PREFIX + "Vous n'avez " + ChatColor.RED + "pas fourni de code valide" + ChatColor.GRAY + ". Tapez \"" + ChatColor.YELLOW + "/link" + ChatColor.GRAY + "\" sur notre serveur Discord pour en recevoir un."))),
+
+    CONNECTED_TO_PROXIMITY_VOICE_CHAT(Maps.newHashMap(ImmutableMap.of(
+            Lang.EN, PREFIX + "You are " + ChatColor.GREEN + "now connected " + ChatColor.GRAY + "to the proximity voice chat.",
+            Lang.FR, PREFIX + "Vous êtes " + ChatColor.GREEN + "désormais connecté " + ChatColor.GRAY + "au chat vocal de proximité."))),
+
+    DISCONNECTED_FROM_PROXIMITY_VOICE_CHAT(Maps.newHashMap(ImmutableMap.of(
+            Lang.EN, PREFIX + "You are " + ChatColor.RED + "now disconnected " + ChatColor.GRAY + "from the proximity voice chat.",
+            Lang.FR, PREFIX + "Vous êtes " + ChatColor.RED + "désormais déconnecté " + ChatColor.GRAY + "du chat vocal de proximité."))),
+
+    ACTION_BAR_ALERT(Maps.newHashMap(ImmutableMap.of(
+            Lang.EN, ChatColor.RED + "⚠ " + ChatColor.GRAY + "You are " + ChatColor.RED + "moving away " + ChatColor.GRAY + "from near players.",
+            Lang.FR, ChatColor.RED + "⚠ " + ChatColor.GRAY + "Vous vous " + ChatColor.RED + "éloignez " + ChatColor.GRAY + "des joueurs à proximité.")));
+
+    final Map<Lang, String> messages;
+
+    Minecraft(Map<Lang, String> messages) {
+        this.messages = messages;
+    }
+
+    @Override
+    public String toString() {
+        return messages.getOrDefault(Lang.valueOf(getPlugin().getConfigFile().getString("language")), messages.get(Lang.EN));
+    }
+}
