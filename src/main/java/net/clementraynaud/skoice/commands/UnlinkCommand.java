0663fdc9123068d962b146e28f485fd468754ba5
Clément Raynaud <30211659+carlodrift@users.noreply.github.com>
2024-01-04 14:39:59 +0100

    Add missing processing in DiscordSRV account link sync

--- src/main/java/net/clementraynaud/skoice/commands/UnlinkCommand.java
+++ src/main/java/net/clementraynaud/skoice/commands/UnlinkCommand.java
@@ -63,7 +63,6 @@ public class UnlinkCommand extends ListenerAdapter {
             }
 
             this.plugin.getLinksYamlFile().unlinkUser(minecraftId);
-            LinkedPlayer.getOnlineLinkedPlayers().removeIf(p -> p.getDiscordId().equals(event.getMember().getId()));
             event.reply(this.plugin.getBot().getMenu("account-unlinked").build()).setEphemeral(true).queue();
 
             OfflinePlayer player = this.plugin.getServer().getOfflinePlayer(UUID.fromString(minecraftId));
